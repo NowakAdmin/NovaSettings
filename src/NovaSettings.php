d70eb21f39efb08e851866fd045a15d3fb5612b1
NowakAdmin <71527799+NowakAdmin@users.noreply.github.com>
2026-01-08 21:59:41 +0100

    Regenerate file with proper letter case

---
+++ src/NovaSettings.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Nowakadmin\NovaSettings;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Menu\MenuSection;
+use Laravel\Nova\Nova;
+use Laravel\Nova\Tool;
+
+class NovaSettings extends Tool
+{
+    /**
+     * Perform any tasks that need to happen when the tool is booted.
+     */
+    public function boot(): void
+    {
+        Nova::mix('novasettings', __DIR__.'/../dist/mix-manifest.json');
+    }
+
+    /**
+     * Build the menu that renders the navigation links for the tool.
+     */
+    public function menu(Request $request): MenuSection
+    {
+        return MenuSection::make('NovaSettings')
+            ->path('/novasettings')
+            ->icon('server');
+    }
+}
